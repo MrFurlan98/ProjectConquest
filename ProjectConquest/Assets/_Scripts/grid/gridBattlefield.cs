0d98665eddfcba73ba4042228c30511aac8688b1
Vinicius Ribeiro Furlan <mr.furlan98@gmail.com>
2021-01-08 22:55:53 +0000

    [ADDED] First attempt to make pieces moves in the  grid

---
+++ ProjectConquest/Assets/_Scripts/grid/gridBattlefield.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gridBattlefield : MonoBehaviour
+{
+
+    public GameObject target;
+    public GameObject monster;
+    Vector3 finalPos;
+    public float cellSize;
+    public float offset;
+
+    void LateUpdate()
+    {
+        finalPos.x = Mathf.Floor( target.transform.position.x / cellSize ) * cellSize + offset;
+        finalPos.y = this.transform.position.y + offset;
+        finalPos.z = Mathf.Floor( target.transform.position.z / cellSize ) * cellSize + offset;
+
+        monster.transform.position = finalPos;
+    }
+}
