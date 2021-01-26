ce0b3fc3b5cc1d833f2525529f89b409c91c1b2c
Vinicius Ribeiro Furlan <mr.furlan98@gmail.com>
2021-01-26 19:03:46 +0000

    [Fix] Moviment is smoother and working properly

---
+++ ProjectConquest/Assets/_Scripts/GameManager/gameController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameController : MonoBehaviour
+{
+    public GameObject currentMosnter;
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        Ray rayPos = Camera.main.ScreenPointToRay(mousePoint);
+        Debug.Log("aqui");
+
+        Physics.Raycast(rayPos, out RaycastHit hit, LayerMask.GetMask("Monster"));
+        if (hit.collider)
+        {
+            Debug.Log("aqui");
+            currentMosnter = hit.transform.gameObject;
+        }
+    }
+}
---
+++ ProjectConquest/Assets/_Scripts/Monster/Monster_Movement/moveMonster.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveMonster : MonoBehaviour
+{
+
+    public GameObject target;
+    Vector3 finalPos;
+    public float cellSize;
+    public float offset;
+
+    private void OnMouseDown()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+
+        Ray rayPos = Camera.main.ScreenPointToRay(mousePoint);
+        Debug.Log("aqui");
+
+
+        Physics.Raycast(rayPos, out RaycastHit hit, LayerMask.GetMask("Monster"));
+        if (hit.collider)
+        {
+            Debug.Log("aqui");  
+            target = hit.transform.gameObject;
+        }
+
+    }
+    private void OnMouseUp()
+    {
+        finalPos.x = Mathf.Floor(target.transform.position.x / cellSize) * cellSize;
+        finalPos.y = this.transform.position.y + offset;
+        finalPos.z = Mathf.Floor(target.transform.position.z / cellSize) * cellSize;
+
+        transform.position = finalPos;
+    }
+}
